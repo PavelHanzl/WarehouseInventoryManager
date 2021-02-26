9a0b54cb4559710f77a2a0cbc4f39e8b7c01bec3
Pavel <hanzlpavel77@gmail.com>
2021-02-26 21:17:40 +0100

    Nadesignována register activity

---
+++ app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package cz.pavelhanzl.warehouseinventorymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.content.Intent
+import android.text.TextUtils
+import com.google.android.gms.tasks.OnCompleteListener
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
