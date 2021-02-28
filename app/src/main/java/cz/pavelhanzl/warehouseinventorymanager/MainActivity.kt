51149750539806fcc5bf253b2412fec60e517480
Pavel <hanzlpavel77@gmail.com>
2021-03-01 00:49:12 +0100

    implementováno drawer menu + přidáno tlačítko pro logout, ale toto tlačítko je zatím nefunkční

--- app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/MainActivity.kt
+++ app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/MainActivity.kt
@@ -3,26 +3,30 @@ package cz.pavelhanzl.warehouseinventorymanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var listener: NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
-
-
-
-
-
-
-
-
-
+        setUpDrawerMenu()
+        setUpActionBarBasedOnNavigation()
 
 
         /*
@@ -40,5 +44,21 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
         */
+    }
+
+    private fun setUpActionBarBasedOnNavigation() {
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setUpDrawerMenu() {
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        drawerNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
