abc277d77c98bba6d6d0c5844e33d2c795643217
Pavel <hanzlpavel77@gmail.com>
2021-03-17 00:00:38 +0100

    funkční nahrávání obrázku ke skladu a do databáze z fragmentu

--- app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/createWarehouse/CreateWarehouseFragment.kt
+++ app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/createWarehouse/CreateWarehouseFragment.kt
@@ -1,11 +1,17 @@
 package cz.pavelhanzl.warehouseinventorymanager.ownWarehouse.createWarehouse
 
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -15,10 +21,14 @@ import androidx.navigation.fragment.findNavController
 import cz.pavelhanzl.warehouseinventorymanager.MainActivity
 import cz.pavelhanzl.warehouseinventorymanager.R
 import cz.pavelhanzl.warehouseinventorymanager.databinding.FragmentCreateWarehouseBinding
+import kotlinx.android.synthetic.main.fragment_create_warehouse.*
 
 class CreateWarehouseFragment : Fragment() {
     private lateinit var binding: FragmentCreateWarehouseBinding
     lateinit var viewModel: CreateWarehouseFragmentViewModel
+    var imageFile: Uri? = null
+    lateinit var obrazekDrawable: Drawable
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +49,42 @@ class CreateWarehouseFragment : Fragment() {
 
 
 
-        viewModel.goBackToPreviousScreen.observe(this, Observer {
+        viewModel.goBackToPreviousScreen.observe(viewLifecycleOwner, Observer {
             if (it) {
-                findNavController().navigate(R.id.action_createWarehouseFragment_to_ownWarehouseFragment)
+                findNavController().navigateUp()
             }
         })
 
+
+        binding.ciWarehouseProfileImageFragmentCreateWarehouse.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+            }
+        }
+
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if( resultCode == Activity.RESULT_OK && requestCode == 0){
+            data?.data.let {
+                imageFile =it
+                ci_WarehouseProfileImage_FragmentCreateWarehouse.setImageURI(imageFile)
+                var inputstream = requireContext().contentResolver.openInputStream(it!!)
+                var bytove = inputstream!!.readBytes()
+                viewModel.warehouseProfilePhoto.value = bytove
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        Log.d("Ničitel","Myších doupat!")
+        super.onDestroy()
+
     }
 
 }
--- app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/createWarehouse/CreateWarehouseFragmentViewModel.kt
+++ app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/createWarehouse/CreateWarehouseFragmentViewModel.kt
@@ -1,6 +1,9 @@
 package cz.pavelhanzl.warehouseinventorymanager.ownWarehouse.createWarehouse
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.Log
+import androidx.core.graphics.drawable.toDrawable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -8,6 +11,9 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import cz.pavelhanzl.warehouseinventorymanager.R
 import cz.pavelhanzl.warehouseinventorymanager.repository.Warehouse
 import cz.pavelhanzl.warehouseinventorymanager.service.BaseViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -20,9 +26,12 @@ class CreateWarehouseFragmentViewModel : BaseViewModel() {
 
     var warehouseNameContent = MutableLiveData<String>("")
     var warehouseNoteContent = MutableLiveData<String>("")
+    var warehouseProfilePhoto = MutableLiveData<ByteArray>()
 
     private val _photoURL = MutableLiveData<String>("")
     val photoURL: LiveData<String> get() = _photoURL
+
+
 
     private val _showCreateAnim = MutableLiveData<Boolean>(false)
     val showCreateAnim: LiveData<Boolean> get() = _showCreateAnim
@@ -39,16 +48,29 @@ class CreateWarehouseFragmentViewModel : BaseViewModel() {
             Log.d("Had", "Klikáš mi na vytvářecího hada, debile")
 
             try {
-                val ref = db.collection("warehouses").document()
+                var profileImageURL : Uri? = null
+                val warehouseDocRef = db.collection("warehouses").document()
+
+
+                if(warehouseProfilePhoto.value != null) {
+                    Log.d("Had", "Snažíš se mi proběhnout, debile")
+                    try {
+                        storage.child("/warehouses/${warehouseDocRef.id}/profileImage.jpg").putBytes(warehouseProfilePhoto.value!!).await()
+                        profileImageURL = storage.child("/warehouses/${warehouseDocRef.id}/profileImage.jpg").downloadUrl.await()
+                    } catch (e: Exception){
+                        Log.d("Uložto", e.message.toString())
+                    }
+
+                }
 
                 val warehouse: MutableMap<String, Any> = HashMap()
-                warehouse["warehouseID"] = ref.id
+                warehouse["warehouseID"] = warehouseDocRef.id
                 warehouse["owner"] = auth.currentUser!!.uid
                 warehouse["name"] = warehouseNameContent.value!!
                 warehouse["note"] = warehouseNoteContent.value!!
-                warehouse["photoURL"] = ""
+                warehouse["photoURL"] = if (profileImageURL!=null) profileImageURL.toString() else ""
 
-                ref.set(warehouse).await()
+                warehouseDocRef.set(warehouse).await()
             } catch (e: Exception) {
                 Log.d("Had", "Něco je špatně: ${e.message}")
                 return@launch
@@ -61,4 +83,5 @@ class CreateWarehouseFragmentViewModel : BaseViewModel() {
         Log.d("Had", "Klikáš mi na zpětného hada, debile")
         _goBackToPreviousScreen.postValue(true)
     }
+
 }
