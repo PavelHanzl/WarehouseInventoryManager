c1a4be9f7eb15cb35a838d506942f33396bde6c8
Pavel <hanzlpavel77@gmail.com>
2021-03-23 01:14:47 +0100

    přidán expandable floating action button na přidání položky do skladu

--- app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/ownWarehouseDetail/OwnWarehouseDetailFragment.kt
+++ app/src/main/java/cz/pavelhanzl/warehouseinventorymanager/ownWarehouse/ownWarehouseDetail/OwnWarehouseDetailFragment.kt
@@ -1,14 +1,19 @@
 package cz.pavelhanzl.warehouseinventorymanager.ownWarehouse.ownWarehouseDetail
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import cz.pavelhanzl.warehouseinventorymanager.MainActivity
+import cz.pavelhanzl.warehouseinventorymanager.R
 import cz.pavelhanzl.warehouseinventorymanager.databinding.FragmentOwnWarehouseDetailBinding
 import cz.pavelhanzl.warehouseinventorymanager.service.BaseFragment
 import kotlinx.android.synthetic.main.menu_header.*
@@ -16,10 +21,15 @@ import kotlinx.android.synthetic.main.menu_header.*
 class OwnWarehouseDetailFragment : BaseFragment() {
     private lateinit var binding: FragmentOwnWarehouseDetailBinding
     lateinit var viewModel: OwnWarehousesDetailFragmentViewModel
+    private val fabAnimFromBottom: Animation by lazy{AnimationUtils.loadAnimation(requireContext(), R.anim.fab_from_bottom)}
+    private val fabAnimToBottom: Animation by lazy{AnimationUtils.loadAnimation(requireContext(), R.anim.fab_to_bottom)}
+
+    private var addItemClicked = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
 
 
         if (savedInstanceState == null) {
@@ -28,7 +38,6 @@ class OwnWarehouseDetailFragment : BaseFragment() {
             val args: OwnWarehouseDetailFragmentArgs by navArgs()
             viewModel.setData(args.warehouseID)
         }
-
 
     }
 
@@ -48,8 +57,63 @@ class OwnWarehouseDetailFragment : BaseFragment() {
                 viewModel.warehouseObject.value!!.name
         })
 
+        binding.fabOwnWhDetailAddItem.setOnClickListener {
+            onAddItemButtonClicked()
+        }
+
+        binding.fabOwnWhDetailAddItemByHand.setOnClickListener {
+            Toast.makeText(requireContext(),"add by hand",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.fabOwnWhDetailAddItemByScan.setOnClickListener {
+            Toast.makeText(requireContext(),"add by scan",Toast.LENGTH_SHORT).show()
+        }
+
 
         return binding.root
+    }
+
+    private fun onAddItemButtonClicked() {
+        setVisibility(addItemClicked)
+        setAnimation(addItemClicked)
+        addItemClicked = !addItemClicked
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if(!clicked){
+            binding.fabOwnWhDetailAddItemByScan.startAnimation(fabAnimFromBottom)
+            binding.fabOwnWhDetailAddItemByHand.startAnimation(fabAnimFromBottom)
+        } else{
+            binding.fabOwnWhDetailAddItemByScan.startAnimation(fabAnimToBottom)
+            binding.fabOwnWhDetailAddItemByHand.startAnimation(fabAnimToBottom)
+        }
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if(!clicked){
+            binding.fabOwnWhDetailAddItemByScan.visibility = VISIBLE
+            binding.fabOwnWhDetailAddItemByHand.visibility = VISIBLE
+        } else{
+            binding.fabOwnWhDetailAddItemByScan.visibility = INVISIBLE
+            binding.fabOwnWhDetailAddItemByHand.visibility = INVISIBLE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.own_warehouse_detail_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            R.id.miOwnWarehouseEdit -> {
+                Toast.makeText(context,"Edit",Toast.LENGTH_SHORT).show()}
+
+            R.id.miOwnWarehouseDelete -> {Toast.makeText(context,"Delete",Toast.LENGTH_SHORT).show()}
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
